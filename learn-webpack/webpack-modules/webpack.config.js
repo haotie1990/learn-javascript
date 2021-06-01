c7eb0de9c8bfddfdcc49cf13bd67dc99599b3931
gengkaiyang <gengkaiyang@vipkid.com.cn>
2021-06-01 17:17:34 +0800

    learn webpack module

---
+++ learn-webpack/webpack-modules/webpack.config.js
@@ -0,0 +1,10 @@
+'use strict';
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  }
+}
