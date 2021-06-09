b72761864cbce61afb89c6227ffd582dd78901fe
gengkaiyang <gengkaiyang@vipkid.com.cn>
2021-06-09 16:50:55 +0800

    lodash get

---
+++ js-interview-question/lodash-get.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * @param {object} content
+ * @param {string/array} path
+ * @param {*} defaultValue
+ */
+function getValue(context, path, defaultValue) {
+  if (Object.prototype.toString.call(context) !== '[object Object]') {
+    return context;
+  }
+  let paths = [];
+  if (Array.isArray(path)) {
+    paths = [...path];
+  } else if (Object.prototype.toString.call(path) === '[object String]') {
+    path.split('.')
+      .map(p => {
+        if (p.includes('[') && p.endsWith(']')) {
+          return [
+            p.slice(0, p.indexOf('[')),
+            p.slice(p.indexOf('[') + 1, p.length - 1)
+          ]
+        }
+        return p;
+      })
+      .forEach(p => {
+        paths = paths.concat(Array.isArray(p) ? p : [p]);
+      });
+  } else {
+    paths = [String(path)];
+  }
+  let result = undefined;
+  for (let i = 0; i < paths.length; i++) {
+    const key = paths[i];
+    result = result ? result[key] : context[key];
+    if (result !== null && typeof result !== 'undefined') {
+      continue;
+    }
+    return defaultValue || undefined;
+  }
+  return result;
+}
+
+const object = { 'a': [{ 'b': { 'c': 3 } }] };
+
+console.log(getValue(object, 'a[0].b.c'));
+console.log(getValue(object, 'a'));
+console.log(getValue(object, ['a', '0', 'b', 'c']));
+console.log(getValue(object, 'a.b.c', 'default'));
