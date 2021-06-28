764ba0943de1b8446666e7f9793aa14966904c2a
gengkaiyang <gengkaiyang@vipkid.com.cn>
2021-06-28 16:35:15 +0800

    function bind

--- js-interview-question/function-bind.js
+++ js-interview-question/function-bind.js
@@ -1,4 +1,11 @@
 
+/**
+ * 1. 创建一个新的函数
+ * 2. 可以传入参数作为原函数的实参
+ * 3. 创建函数可以作为构造函数但传入this失效
+ * @param {*} ctx 
+ * @returns 
+ */
 Function.prototype.Bind = function bind(ctx) {
   if (typeof this !== "function") {
     throw Error("not a function");
@@ -6,13 +13,19 @@ Function.prototype.Bind = function bind(ctx) {
   let fn = this;
   let args = [...arguments].slice(1);
 
+  let fnNop = function() {};
+
   let resFn = function() {
     // bind创建的函数作为构造函数出现时，bind传入的this对象将失效
     let self = this instanceof resFn ? this : ctx; // ???
     return fn.apply(self, args.concat([...arguments]));
   };
   // 保证bind创建的函数可以作为构造函数使用且会继承原函数的原型链
-  resFn.prototype = this.prototype;
+  // 问题：修改绑定函数的prototype会造成原函数的prototype一并被修改
+  // resFn.prototype = this.prototype;
+
+  fnNop.prototype = this.prototype;
+  resFn.prototype = new fnNop();
   return resFn;
 }
 
