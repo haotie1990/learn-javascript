70e3b10e29c80d0cececdef2b12f3e4f6f395637
gengkaiyang <gengkaiyang@vipkid.com.cn>
2021-07-14 19:41:41 +0800

    add:class-extends-es5-babel

---
+++ js-interview-question/class-extends-es5-babel.js
@@ -0,0 +1,205 @@
+/**
+ * 
+ * class P {
+ *   constructor(name) {
+ *     this.name = name;
+ *   }
+ *   log() {
+ *    console.log('log');
+ *   }
+ *   static print() {
+ *     console.log('print');
+ *   }
+ * }
+ * 
+ * class Q exteds P {
+ *   constructor(name) {
+ *     super(name);
+ *   }
+ * }
+ *  
+ */
+
+ function _typeof(obj) {
+  "@babel/helpers - typeof";
+  if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
+    _typeof = function _typeof(obj) {
+      return typeof obj;
+    };
+  } else {
+    _typeof = function _typeof(obj) {
+      return obj &&
+        typeof Symbol === "function" &&
+        obj.constructor === Symbol &&
+        obj !== Symbol.prototype
+        ? "symbol"
+        : typeof obj;
+    };
+  }
+  return _typeof(obj);
+}
+
+function _instanceof(left, right) {
+  if (
+    right != null &&
+    typeof Symbol !== "undefined" &&
+    right[Symbol.hasInstance]
+  ) {
+    return !!right[Symbol.hasInstance](left);
+  } else {
+    return left instanceof right;
+  }
+}
+
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function");
+  }
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: { value: subClass, writable: true, configurable: true }
+  });
+  // ? 为什么还要设置__proto__
+  // 静态方法可以被子类继承
+  if (superClass) _setPrototypeOf(subClass, superClass);
+}
+
+function _setPrototypeOf(o, p) {
+  _setPrototypeOf =
+    Object.setPrototypeOf ||
+    function _setPrototypeOf(o, p) {
+      o.__proto__ = p;
+      return o;
+    };
+  return _setPrototypeOf(o, p);
+}
+
+function _createSuper(Derived) {
+  var hasNativeReflectConstruct = _isNativeReflectConstruct();
+  return function _createSuperInternal() {
+    var Super = _getPrototypeOf(Derived),
+      result;
+    if (hasNativeReflectConstruct) {
+      var NewTarget = _getPrototypeOf(this).constructor;
+      result = Reflect.construct(Super, arguments, NewTarget);
+    } else {
+      result = Super.apply(this, arguments);
+    }
+    return _possibleConstructorReturn(this, result);
+  };
+}
+
+function _possibleConstructorReturn(self, call) {
+  if (call && (_typeof(call) === "object" || typeof call === "function")) {
+    return call;
+  }
+  return _assertThisInitialized(self);
+}
+
+function _assertThisInitialized(self) {
+  if (self === void 0) {
+    throw new ReferenceError(
+      "this hasn't been initialised - super() hasn't been called"
+    );
+  }
+  return self;
+}
+
+function _isNativeReflectConstruct() {
+  if (typeof Reflect === "undefined" || !Reflect.construct) return false;
+  if (Reflect.construct.sham) return false;
+  if (typeof Proxy === "function") return true;
+  try {
+    Boolean.prototype.valueOf.call(
+      Reflect.construct(Boolean, [], function () {})
+    );
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function _getPrototypeOf(o) {
+  _getPrototypeOf = Object.setPrototypeOf
+    ? Object.getPrototypeOf
+    : function _getPrototypeOf(o) {
+        return o.__proto__ || Object.getPrototypeOf(o);
+      };
+  return _getPrototypeOf(o);
+}
+
+function _classCallCheck(instance, Constructor) {
+  if (!_instanceof(instance, Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+var P = /*#__PURE__*/ (function () {
+  "use strict";
+
+  function P(name) {
+    _classCallCheck(this, P);
+
+    this.name = name;
+  }
+
+  _createClass(
+    P,
+    [
+      {
+        key: "log",
+        value: function log() {
+          console.log("log");
+        }
+      }
+    ],
+    [
+      {
+        key: "print",
+        value: function print() {
+          console.log("print");
+        }
+      }
+    ]
+  );
+
+  return P;
+})();
+
+var Q = /*#__PURE__*/ (function (_P) {
+  "use strict";
+
+  _inherits(Q, _P);
+
+  var _super = _createSuper(Q);
+
+  function Q(name) {
+    // class类不能作为函数调用，只能作为构造函数
+    _classCallCheck(this, Q);
+
+    return _super.call(this, name);
+  }
+
+  return Q;
+})(P);
+
+
+const q = new Q('Q');
+console.log(q.name);
+q.log();
+Q.print();
