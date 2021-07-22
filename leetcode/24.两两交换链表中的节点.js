222119927457b60c930314e6a470182236cc7e27
gengkaiyang <gengkaiyang@vipkid.com.cn>
2021-07-22 10:18:39 +0800

    update

---
+++ leetcode/24.两两交换链表中的节点.js
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=javascript
+ *
+ * [24] 两两交换链表中的节点
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (69.99%)
+ * Likes:    972
+ * Dislikes: 0
+ * Total Accepted:    281.8K
+ * Total Submissions: 402.7K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * 
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：head = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能在不修改链表节点值的情况下解决这个问题吗?（也就是说，仅修改节点本身。）
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+  if (!head || !head.next) {
+    return head;
+  }
+  let pre = head;
+  let cur = head.next;
+  pre.next = swapPairs(cur.next);
+  cur.next = pre;
+  return cur;
+};
+// @lc code=end
+
